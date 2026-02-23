8f12776b11340074fd9584be5dcc270a45ea0043
kembo <asininefatuity@gmail.com>
2026-02-23 18:03:39 +0300

    feat: wrote node logic

--- mcp_client.py
+++ mcp_client.py
@@ -1,11 +1,14 @@
 from typing import Annotated, List 
 from typing_extensions import TypedDict
 from decouple import config
+from mcp import ClientSession, StdioServerParameters
 from langgraph.graph.message import AnyMessage, add_messages
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_mcp_adapters.tools import load_mcp_tools
-from mcp import ClientSession, StdioServerParameters
+from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import ToolNode, tools_condition
 
 server_params = StdioServerParameters(command="uv run mcp_server.py")
 client = ClientSession(server_params)
@@ -27,3 +30,17 @@ async def create_graph(session):
         MessagesPlaceholder(variable_name="messages")
     ])
     chat_llm = prompt_template | llm_with_tools
+
+
+    def chat_node(state: LangGraphState) -> LangGraphState:
+        state["messages"] = chat_llm.invoke({"messages": state["messages"]})
+        return state 
+    
+    #Build Langraph with tool routing 
+    graph=StateGraph(LangGraphState)
+    graph.add_node("chat_node", chat_node)
+    graph.add_node("tool_node", ToolNode(tools=tools))
+    graph.add_edge(START, "chat_node")
+    graph.add_conditional_edges("chat_node", tools_condition, {"tools": "tool_node", "__end__": END})
+    graph.add_edge("tool_node", "chat_node")
+    return graph.compile(checkpointer=MemorySaver())
